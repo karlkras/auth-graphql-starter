f1198edd0f075473c256c97e704377d318e8db1c
kkras2 <karl.krasnowsky@nike.com>
2017-08-21 07:32:22 -0700

    first setup iteration.

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://karlkras:karlkras@ds151153.mlab.com:51153/auth_for_me';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
